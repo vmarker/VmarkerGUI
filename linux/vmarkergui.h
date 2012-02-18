085c83ec13e4adf2c1d12e85dcdd24d788f977fd
Robin Theunis <robin@vmarker.org>
2012-02-18 10:12:44 +0100

    Added license

--- linux/vmarkergui.h
+++ linux/vmarkergui.h
@@ -1,3 +1,11 @@
+/* VmarkerGUI by Robin Theunis 
+ * 2011-2012 
+ * For use with the vmarker IR sensor */
+
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 #ifndef VMARKERGUI_H
 #define VMARKERGUI_H
 
